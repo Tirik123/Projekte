729df56d959fb9ffe5bf1fb72da096e94f7c7518
Tirik123 <claudiocheri00@gmail.com>
2023-09-25 17:26:14 +0200

    ed

--- FirstMySQLConnection.session.sql
+++ FirstMySQLConnection.session.sql
@@ -1,4 +1,7 @@
-SELECT * FROM users;
-
-
+CREATE TABLE Produkte (
+    product_id INT AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(150) NOT NULL,
+    product_price FLOAT(10, 2),
+    PRIMARY KEY (product_id)
+);
 
