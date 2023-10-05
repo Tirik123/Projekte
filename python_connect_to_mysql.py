a2f4fa642ce87c25a7e2c15d43b6180a6007151c
Tirik123 <claudiocheri00@gmail.com>
2023-10-05 15:01:06 +0200

    ed

--- python_connect_to_mysql.py
+++ python_connect_to_mysql.py
@@ -9,6 +9,22 @@ my_cursor = conn.cursor()
 
 #my_cursor.execute("ALTER TABLE Warenkorb ADD COLUMN product_amount INT NOT NULL")
 
-my_cursor.execute('INSERT INTO Produkte (product_name, product_price) VALUES (%s, %s)', ("Aubergine", 2.99))
+#my_cursor.execute('INSERT INTO Produkte (product_name, product_price) VALUES (%s, %s)', ("Aubergine", 2.99))
 
-conn.commit()
+#conn.commit()
+
+#my_cursor.execute('Select * From Produkte')
+
+#my_cursor.execute("INSERT INTO produkte ")
+
+'''for x in my_cursor:
+    print(x)'''
+
+#my_cursor.execute('INSERT INTO Produkte (product_name, product_price) VALUES (%s, %s)', ("Apfel", 1.99))
+
+#conn.commit()
+
+my_cursor.execute('Select * From Produkte')
+
+for x in my_cursor:
+    print(x)
